1d56e19ab677be0d640fec3a0abed4c996804f11
tinnathuridur <tinnath@Tinnas-MacBook-Pro.local>
2017-02-25 18:18:08 +0000

    fixed find fit

--- mm.c
+++ mm.c
@@ -527,7 +527,7 @@ static void *find_fit(size_t asize)
     /* first fit search */
     void *bp;
 
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+    for (bp = heap_start; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
             return bp;
         }
